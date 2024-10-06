5e1aaed1b1ff5453e1d0f756fc9e5c42aa7608f3
predrag-arsic <predrag87arsic@gmail.com>
2024-10-06 10:32:38 +0200

    INIT

---
+++ src/main/java/org/apiexport/apiexport/ApiExportApplication.java
@@ -0,0 +1,13 @@
+package org.apiexport.apiexport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/apiexport/apiexport/ApiExportApplicationTests.java
@@ -0,0 +1,13 @@
+package org.apiexport.apiexport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
